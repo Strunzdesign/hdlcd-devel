cf9f60ce19b1f41e4430edcceeca60c29fdedd5b
Florian Evers <florian-evers@gmx.de>
2016-10-08 12:41:14 +0200

    Changed INSTALL file reflecting use of git submodules

--- src/HdlcdConfig.h
+++ src/HdlcdConfig.h
@@ -1,3 +1,3 @@
 // The configured options and settings for the HDLC Daemon includes
 #define HDLCD_DEVEL_VERSION_MAJOR "1"
-#define HDLCD_DEVEL_VERSION_MINOR "0"
+#define HDLCD_DEVEL_VERSION_MINOR "1pre"
