01d688972f10c44a40b895eb1c45b31852d0b7af
Florian Evers <florian-evers@gmx.de>
2016-11-22 22:25:32 +0100

    Preparations for tag v1.2

--- src/HdlcdConfig.h
+++ src/HdlcdConfig.h
@@ -38,4 +38,4 @@
 
 // The configured options and settings for the HDLC Daemon includes
 #define HDLCD_DEVEL_VERSION_MAJOR "1"
-#define HDLCD_DEVEL_VERSION_MINOR "1pre"
+#define HDLCD_DEVEL_VERSION_MINOR "2"
